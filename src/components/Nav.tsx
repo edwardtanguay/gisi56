8a5e0369dfa88bacea6c3addbfe65220eb79a2e0
Edward Tanguay (windows) <edward@tanguay.info>
2024-09-01 11:11:20 +0200

    next large

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,7 @@
 import { NavLink } from "react-router-dom";
 
 export const Nav = () => {
-	return <nav className="flex justify-end">
+	return <nav className="flex justify-end mt-2 mr-2">
 		<button><NavLink to="/page002">next</NavLink></button>
 	</nav>;
 };
