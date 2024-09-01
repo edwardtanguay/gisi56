f5375b7a5edcccaa99371455e5ccc5c8a00091fd
Edward Tanguay (windows) <edward@tanguay.info>
2024-09-01 11:32:50 +0200

    click through works

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,16 +1,28 @@
 import { NavLink, useLocation } from "react-router-dom";
 
+const pages = [
+	"welcome",
+	"page002",
+	"page003"
+];
+
 
 export const Nav = () => {
-  const location = useLocation();
+	const location = useLocation();
 
-  const path = location.pathname;
-  const segments = path.split('/').filter(Boolean);
-  const pageId = segments.length > 0 ? segments[segments.length - 1] : null;
-  
+	const getNextPageId = () => {
+		const path = location.pathname;
+		const segments = path.split('/').filter(Boolean);
+		const currentPageId = segments.length > 0 ? segments[segments.length - 1] : "welcome";
+		const currentIndex = pages.indexOf(currentPageId);
+		if (currentIndex === -1 || currentIndex === pages.length - 1) {
+			return 'welcome';
+		}
+		return pages[currentIndex + 1];
+	}
+	const nextPageId = getNextPageId();
 
 	return <nav className="flex justify-end mt-2 mr-2">
-		[{pageId}]
-		<button><NavLink to="/page002">next</NavLink></button>
+		<button><NavLink to={nextPageId}>next</NavLink></button>
 	</nav>;
 };
