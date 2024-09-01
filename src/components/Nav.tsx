f4a3d5e637b2dabbf36e2c6c7117c6bcb783bbaa
Edward Tanguay (windows) <edward@tanguay.info>
2024-09-01 11:21:07 +0200

    showing page

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,16 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
+
 
 export const Nav = () => {
+  const location = useLocation();
+
+  const path = location.pathname;
+  const segments = path.split('/').filter(Boolean);
+  const pageId = segments.length > 0 ? segments[segments.length - 1] : null;
+  
+
 	return <nav className="flex justify-end mt-2 mr-2">
+		[{pageId}]
 		<button><NavLink to="/page002">next</NavLink></button>
 	</nav>;
 };
