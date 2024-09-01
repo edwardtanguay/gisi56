5fc7bba132951776da91f6f87e2b0a4f3c2aa66a
Edward Tanguay (windows) <edward@tanguay.info>
2024-09-01 13:05:47 +0200

    main

--- src/pages/Page003.tsx
+++ src/pages/Page003.tsx
@@ -1,5 +1,7 @@
 export const Page003 = () => {
 	return (
-		<p>As we know, every year of life brings its own challenges.</p>
+		<main>
+			<p>As we know, every year of life brings its own challenges.</p>
+		</main>
 	)
 }
