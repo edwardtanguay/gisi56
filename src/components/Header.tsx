850d23dcf89e02b3ca004579078fe54e5347542f
Edward Tanguay (windows) <edward@tanguay.info>
2024-09-01 10:53:16 +0200

    page002

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,6 @@ import { Nav } from "./Nav";
 export const Header = () => {
 	return (
 		<>
-			<h1 className="text-3xl mb-3 text-slate-800">Info Site</h1>
 			<Nav/>
 		</>
 	);
---
+++ src/pages/Page002.tsx
@@ -0,0 +1,5 @@
+export const Page002 = () => {
+	return (
+		<p>page002</p>
+	)
+}
